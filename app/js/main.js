0ce1808c1ed788c7d8ca0ddd9a466ba5f4830b3c
Cheh91 <4eh91@ukr.net>
2020-05-28 00:13:08 +0300

    added serf

--- app/js/main.js
+++ app/js/main.js
@@ -5,14 +5,30 @@ $(function(){
         fade: true,
         prevArrow: '<img class="slider__arrows slider__arrows-left" src="img/arrows-left.svg" alt="">',
         nextArrow: '<img class="slider__arrows slider__arrows-right" src="img/arrows-right.svg" alt="">',
-        asNavFor: ".slider-dots",
+        asNavFor: ".slider-dotshead",
     });
 
-    $(".slider-dots").slick({
+    $(".slider-dotshead").slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: ".header__slider",
     });
 
-  
+
+    $(".serf-slider").slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider__arrows slider__arrows-left" src="img/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider__arrows slider__arrows-right" src="img/arrows-right.svg" alt="">',
+        asNavFor: ".slider-map",
+    });
+
+
+    $(".slider-map").slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: ".serf-slider",
+        focusOnSelect: true,
+    })
 });
