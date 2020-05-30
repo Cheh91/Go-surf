78fc992c3415502d658efad541ae7081f16965ff
Cheh91 <4eh91@ukr.net>
2020-05-30 12:11:07 +0300

    added sleep

--- app/js/main.js
+++ app/js/main.js
@@ -32,12 +32,54 @@ $(function(){
         focusOnSelect: true,
     });
 
-    $(".travel__slider").slick({
+    $(".holder__slider").slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         prevArrow: '<img class="slider__arrows slider__arrows-left" src="img/arrows-left.svg" alt="">',
         nextArrow: '<img class="slider__arrows slider__arrows-right" src="img/arrows-right.svg" alt="">',
         infinite: true,
         fade: true,
-    })
+    });
+
+
+    jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.png" alt=""></div><div class="quantity-button quantity-down"><img src="img/minus.png" alt=""></div></div>').insertAfter('.quantity input');
+    jQuery('.quantity').each(function() {
+      var spinner = jQuery(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+    });
+
+    let summ = $(".nights").val() * $(".summ").data("nights") + ($(".guests").val() - 1) * $(".summ").data("guests");
+    $(".summ").html("$" + summ);
+
+    $(".quantity-button").on("click", function(){
+        let summ = $(".nights").val() * $(".summ").data("nights") + ($(".guests").val() - 1) * $(".summ").data("guests");
+        $(".summ").html("$" + summ);
+    });
 });
